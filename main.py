f93f64584e84cdf6a771b9c9d9240109e23643d8
aptopcoder <aptopcoder@gmail.com>
2024-02-04 17:42:42 +0530

    Update title

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(title="FastAPI Demo")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
